a236facfe638eedf7da8601d6f080e6de360e793
ry.snowden <kang.yueping@immomo.com>
2020-06-12 21:11:08 +0800

    ok

--- build-image/sql-client/sql-client.sh
+++ build-image/sql-client/sql-client.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-${FLINK_HOME}/bin/sql-client.sh embedded -d /opt/sql-client/sql-training/build-image/conf/conf/sql-client-conf-ali.yaml -l ${SQL_CLIENT_HOME}/lib
+/opt/flink/bin/sql-client.sh embedded -d /opt/sql-client/sql-training/build-image/conf/conf/sql-client-conf-ali.yaml -l /opt/sql-client/lib
